209bcf4f3c2e25ee4b8899ba5211eb9e24581854
Gabe Morris <gabemorris1231@gmail.com>
2023-09-25 17:49:37 -0500

    added interpolation, lu3, and root finding

--- eng_analysis/__init__.py
+++ eng_analysis/__init__.py
@@ -1,5 +1,7 @@
 from .linear_solvers import gauss_solve, choleski_decomposition, choleski_solve, lu_decomposition, lu_solve
 from .iterative_solvers import conj_grad
+from .interpolation import NewtonPoly, CubicSpline
+from .roots import newton_raphson
 
 __all__ = [
     'gauss_solve',
@@ -7,5 +9,8 @@ __all__ = [
     'choleski_solve',
     'lu_decomposition',
     'lu_solve',
-    'conj_grad'
+    'conj_grad',
+    'NewtonPoly',
+    'CubicSpline',
+    'newton_raphson'
 ]
