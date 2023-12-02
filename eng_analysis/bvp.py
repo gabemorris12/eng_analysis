cf1d3ebed3f77ea054c304037dfb5a9a011003ae
Gabe Morris <gabemorris1231@gmail.com>
2023-12-02 16:33:14 -0600

    to be worked on in the future

--- eng_analysis/bvp.py
+++ eng_analysis/bvp.py
@@ -1,9 +1,7 @@
 """
 Submodule for implementing the different boundary value problems.
 """
-from scipy.integrate import solve_bvp
 
 
-def bol_stoer(fun, bc, x, y, **kwargs):
-    sol = solve_bvp(fun, bc, x, y, **kwargs)
-    return sol.sol(x)
+def bol_stoer():
+    raise NotImplementedError('To be added later')
